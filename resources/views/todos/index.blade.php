2495b0046e8054221905ec2e5557328a7475b391
Ehoey <a5607a8776@gmail.com>
2019-04-18 16:47:49 +0800

    create a layouts

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,35 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  crossorigin="anonymous">
-    <title>Todos</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1 class="text-center my-5">Todos Page</h1>
-    <div class="container">
-        <div class="row justify-content-center my-5">
-            <div class="col-md-8 ">
-                <div class="card card-default">
-                    <div class="card-header text-center">
-                        Todo
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            @foreach($todos as $todo)
-                                <li class="list-group-item">
-                                    {{$todo->name}}
-                                    <a href="/todos/{{$todo->id}}" class="btn btn-primary float-right btn-sm">view</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+    <div class="row justify-content-center my-5">
+        <div class="col-md-8 ">
+            <div class="card card-default">
+                <div class="card-header text-center">
+                    Todo
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($todos as $todo)
+                            <li class="list-group-item">
+                                {{$todo->name}}
+                                <a href="/todos/{{$todo->id}}" class="btn btn-primary float-right btn-sm">view</a>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,16 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  crossorigin="anonymous">
+@extends('layouts.app')
 
-    <title>Todo item</title>
-</head>
-<body>
-<div class="container">
+@section('content')
     <h1 class="text-center my-5">
         {{$todos->name}}
     </h1>
@@ -26,6 +16,4 @@
             </div>
         </div>
     </div>
-</div>
-</body>
-</html>
+@endsection
